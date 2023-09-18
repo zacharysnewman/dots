0924473d6bcd13a221426396f5c3af1d4ad9b67a
Zack Newman <znewman@indeed.com>
2023-09-18 15:48:10 -0700

    Updated braille dictionary generation with correct bit order and exported to map

--- src/generateBrailleDictionary.js
+++ src/generateBrailleDictionary.js
@@ -1,20 +1,33 @@
-// Generate the Braille dictionary in order of byte value and with byte literal keys
-const brailleDictionary = {};
+// Generate the Braille dictionary with modified bit order
+const brailleEntries = [];
 for (let i = 0; i <= 0xff; i++) {
-  const binaryString = i.toString(2).padStart(8, "0");
+  // Get the bits in the modified order
+  const bit1 = (i >> 0) & 0x01;
+  const bit2 = (i >> 1) & 0x01;
+  const bit3 = (i >> 2) & 0x01;
+  const bit4 = (i >> 3) & 0x01;
+  const bit5 = (i >> 4) & 0x01;
+  const bit6 = (i >> 5) & 0x01;
+  const bit7 = (i >> 6) & 0x01;
+  const bit8 = (i >> 7) & 0x01;
+
+  // Create the modified binary representation
+  const binaryString = `0b${bit8}${bit7}${bit6}${bit3}${bit5}${bit2}${bit4}${bit1}`;
   const brailleChar = String.fromCharCode(0x2800 + i);
-  brailleDictionary[`0b${binaryString}`] = brailleChar;
+  brailleEntries.push([binaryString, brailleChar]);
 }
 
-// Output the dictionary as a TypeScript object
-console.log("const brailleDictionary = {");
-for (const key in brailleDictionary) {
-  console.log(`  ${key}: '${brailleDictionary[key]}',`);
+// Output the dictionary as a TypeScript Map
+console.log("export const brailleDictionary: Map<number, string> = new Map([");
+for (const entry of brailleEntries) {
+  console.log(`  [${entry[0]}, "${entry[1]}"],`);
 }
-console.log("};");
+console.log("]);");
 
 // Example usage:
 const byteValue = "0b00000001";
 console.log(
-  `Example usage: brailleDictionary[${byteValue}] = ${brailleDictionary[byteValue]}`
+  `Example usage: brailleDictionary.get(${byteValue}) = ${
+    brailleEntries.find((entry) => entry[0] === byteValue)[1]
+  }`
 );
