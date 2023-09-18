36a1318d9b33cd9147d6a4688d01210df4b26e6c
Zack Newman <znewman@indeed.com>
2023-09-18 16:08:37 -0700

    Added app and braille dictionary

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { brailleDictionary } from "./brailleDictionary";
+
 function screenToByteArray(
   screen: number[][],
   screenWidth: number,
@@ -33,7 +35,7 @@ function byteArrayToBrailleString(byteArray: number[][]): string {
 
   for (let y = 0; y < byteArray.length; y++) {
     for (let x = 0; x < byteArray[y].length; x++) {
-      const brailleChar = String.fromCharCode(0x2800 + byteArray[y][x]);
+      const brailleChar = brailleDictionary.get(byteArray[y][x]); //String.fromCharCode(0x2800 + byteArray[y][x]);
       braille += brailleChar;
     }
     braille += "\n"; // Add a newline character after each row.
@@ -41,19 +43,94 @@ function byteArrayToBrailleString(byteArray: number[][]): string {
 
   return braille;
 }
+class GameObject {
+  x: number;
+  y: number;
+  pixel: number;
 
-// Define your 100x100 screen as a 2D array of 1s (on) and 0s (off)
+  constructor(x: number, y: number, pixel: number = 0) {
+    this.x = x;
+    this.y = y;
+    this.pixel = pixel;
+  }
+}
+
+class ScreenBuffer {
+  width: number;
+  height: number;
+  pixels: number[][];
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.pixels = Array.from({ length: height }, () => Array(width).fill(0));
+  }
+
+  renderObject(object: GameObject) {
+    if (
+      object.x >= 0 &&
+      object.x < this.width &&
+      object.y >= 0 &&
+      object.y < this.height
+    ) {
+      this.pixels[object.y][object.x] = object.pixel;
+    }
+  }
+
+  clear() {
+    this.pixels = Array.from({ length: this.height }, () =>
+      Array(this.width).fill(0)
+    );
+  }
+
+  render() {
+    // Convert the pixel array to a 2D byte array
+    const byteArray = screenToByteArray(this.pixels, this.width, this.height);
+
+    // Convert the 2D byte array to a Braille string
+    const brailleString = byteArrayToBrailleString(byteArray);
+
+    // Output the Braille string to the console
+    console.log(brailleString);
+  }
+}
+
+// Example usage:
 const screenWidth = 128;
 const screenHeight = 64;
-const screenBuffer: number[][] = Array.from({ length: screenHeight }, () =>
-  Array(screenWidth).fill(0)
-);
+const screenBuffer = new ScreenBuffer(screenWidth, screenHeight);
 
-// Convert the screen to a 2D byte array
-const byteArray = screenToByteArray(screenBuffer, screenWidth, screenHeight);
+// // Create a GameObject and render it
+// for (let x = 0; x < 2; x++) {
+//   for (let y = 0; y < 4; y++) {
+//     screenBuffer.renderObject(new GameObject(x + 18, y + 18, 1));
+//   }
+// }
 
-// Convert the 2D byte array to a Braille string
-const brailleString = byteArrayToBrailleString(byteArray);
+function clearTerminal(): void {
+  const clearScreen = "\x1B[2J\x1B[H"; // Clear terminal screen
+  process.stdout.write(clearScreen);
+}
 
-// Output the Braille string to the console
-console.log(brailleString);
+function waitForMs(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+async function main() {
+  for (let y = 0; y < screenHeight; y++) {
+    for (let x = 0; x < screenWidth; x++) {
+      clearTerminal();
+      screenBuffer.clear();
+      screenBuffer.renderObject(new GameObject(x, y, 1));
+      screenBuffer.render();
+      console.log({ x, y });
+      await waitForMs(10);
+    }
+  }
+}
+
+main().then(() => {});
